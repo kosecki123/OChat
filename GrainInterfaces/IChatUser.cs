1fe8bfcea2cd0af1ac9fd8981f9545f21bcf2fe1
kosecki123 <piotrkosinski@gmail.com>
2015-01-29 20:53:46 +0100

    OChat spike

---
+++ GrainInterfaces/IChatUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using Orleans;
+
+namespace GrainInterfaces
+{
+    /// <summary>
+    /// Orleans grain communication interface IGrain1
+    /// </summary>
+    public interface IChatUser : Orleans.IGrain
+    {
+        Task SendMessage(Guid receiver, string message);
+        Task ReceiveMessage(Guid sender, string message);
+    }
+}
---
+++ Grains/ChatUser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using Orleans;
+using GrainInterfaces;
+
+namespace Grains
+{
+    /// <summary>
+    /// Orleans grain implementation class ChatUser.
+    /// </summary>
+    public class ChatUser : Orleans.Grain, IChatUser
+    {
+        public Task SendMessage(Guid receiver, string message)
+        {
+            Console.WriteLine("Sending message \"{0}\" to {1}", message, receiver);
+
+            var receiverGrain = ChatUserFactory.GetGrain(receiver);
+            var myself = this.GetPrimaryKey();
+
+            return receiverGrain.ReceiveMessage(myself, message);
+        }
+        
+        public Task ReceiveMessage(Guid sender, string message)
+        {
+            Console.WriteLine("Message \"{0}\" received from {1}", message, sender);
+
+            return TaskDone.Done;
+        }
+    }
+}
